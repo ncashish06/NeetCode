1d3f18bd4b21d00ceb15d91cc37b3329c93d0d4a
Ashish Nadadur Chakravarthi <66215582+ncashish06@users.noreply.github.com>
2026-04-12 00:23:51 -0700

    Add: missing-number - submission-0

---
+++ Data Structures & Algorithms/missing-number/submission-0.py
@@ -0,0 +1,11 @@
+class Solution:
+    # Date Solved: 12 April 2026, Sunday
+    def missingNumber(self, nums: List[int]) -> int:
+        n = len(nums)
+        total_sum = n * (n + 1) // 2
+        partial_sum = 0
+        for i in range(n):
+            partial_sum += nums[i]
+
+        return total_sum - partial_sum
+        
