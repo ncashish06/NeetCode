920e2c1ea2e89bd0474ff6135b20d67dd03a350a
Ashish Nadadur Chakravarthi <66215582+ncashish06@users.noreply.github.com>
2026-05-22 18:46:16 -0700

    Add: check-if-array-is-sorted-and-rotated - submission-1

---
+++ Data Structures & Algorithms/check-if-array-is-sorted-and-rotated/submission-1.py
@@ -0,0 +1,16 @@
+class Solution:
+    # Date Solved: 22 May 2026, Friday
+    def check(self, nums: List[int]) -> bool:
+        # Approach 1: Count Peek/Pivot/Rotation Points (Only 1 peek element/index should be present)
+        n = len(nums)
+        peak = 0
+
+        for i in range(n):
+            # Compare each element with the next one (wrap around using modulo)
+            if nums[i] > nums[(i + 1) % n]:
+                peak += 1
+
+            if peak > 1:
+                return False
+
+        return True
