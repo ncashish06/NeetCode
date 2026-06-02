176feec45932039e0d8a641041942ef962e4b7f7
Ashish Nadadur Chakravarthi <66215582+ncashish06@users.noreply.github.com>
2026-06-02 20:31:25 +0200

    Add: design-hashmap - submission-1

---
+++ Data Structures & Algorithms/design-hashmap/submission-1.py
@@ -0,0 +1,23 @@
+class MyHashMap:
+    # Date Solved: 2 June 2026, Tuesday
+    # NC250
+    # Not the correct way, need to use array/linked list
+    def __init__(self):
+        self.hashmap = {}
+
+    def put(self, key: int, value: int) -> None:
+        self.hashmap[key] = value
+
+    def get(self, key: int) -> int:
+        return self.hashmap.get(key, -1)
+
+    def remove(self, key: int) -> None:
+        if key in self.hashmap:
+            del self.hashmap[key]
+
+
+# Your MyHashMap object will be instantiated and called as such:
+# obj = MyHashMap()
+# obj.put(key,value)
+# param_2 = obj.get(key)
+# obj.remove(key)
