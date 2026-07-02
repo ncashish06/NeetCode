1525ff6a1c18569ee880c1126c248a9686de581b
Ashish Nadadur Chakravarthi <66215582+ncashish06@users.noreply.github.com>
2026-07-02 22:01:31 +0200

    Add: find-the-index-of-the-first-occurrence-in-a-string - submission-1

---
+++ Data Structures & Algorithms/find-the-index-of-the-first-occurrence-in-a-string/submission-1.py
@@ -0,0 +1,49 @@
+class Solution:
+    # Date Solved: 1 July 2026, Wednesday
+    # Refer: Namaste DSA (Two Pointers and Sliding Window)
+    # Not in NC250 but leads to KMP algorithm
+    def strStr(self, haystack: str, needle: str) -> int:
+        """
+        # Approach 1: Brute force, Time: O(n*m)
+        n = len(haystack)
+        m = len(needle)
+
+        for i in range(n - m + 1):
+            j = 0
+            for j in range(m):
+                if haystack[i + j] != needle[j]:
+                    break
+            else:
+                return i
+        return -1
+        """
+        # Approach 2: KMP, Time: O(n+m) = O(n), Space: O(m)
+        n = len(haystack)
+        m = len(needle)
+        lps = [0] * m
+        i = 0
+        j = 1
+        while j < m:
+            if needle[i] == needle[j]:
+                i += 1
+                lps[j] = i
+                j += 1
+            else:
+                if i == 0:
+                    lps[j] = 0
+                    j += 1
+                else:
+                    i = lps[i - 1]
+        i = j = 0
+        while i < n:
+            if haystack[i] == needle[j]:
+                i += 1
+                j += 1
+            else:
+                if j == 0:
+                    i += 1
+                else:
+                    j = lps[j - 1]
+            if j == m:
+                return i - m
+        return -1
