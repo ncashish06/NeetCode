7f1578abaa50e7b0415a1f0d1976e7eac8e26ed0
Ashish Nadadur Chakravarthi <66215582+ncashish06@users.noreply.github.com>
2026-07-10 11:49:58 +0200

    Add: merge-intervals - submission-6

---
+++ Data Structures & Algorithms/merge-intervals/submission-6.py
@@ -0,0 +1,24 @@
+class Solution:
+    # Date Solved: 10 July 2026, Friday
+    # Blind 75
+    # Refer: Namaste DSA
+    # Asked in Master Electronics Interview in March 2026
+    # Time: O(nlogn) for sorting, Space: O(n) for ans array
+    def merge(self, arr: List[List[int]]) -> List[List[int]]:
+        """
+        Approach: Foundation for LC 759 Employee Free Time.
+        LC56:  sort + merge overlapping intervals -> return merged spans
+        LC759: sort + merge overlapping intervals -> return the GAPS between merged spans
+        """
+        arr.sort(key=lambda x: x[0])
+        ans = [arr[0]]
+
+        for i in range(1, len(arr)):
+            start, end = arr[i][0], arr[i][1]
+            last_end = ans[-1][1]
+
+            if start <= last_end:
+                ans[-1][1] = max(last_end, end)
+            else:
+                ans.append([start, end])
+        return ans
